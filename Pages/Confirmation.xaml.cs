4d66ebe1b047fce8c43299212eb4c8708d77fcb1
emptiness <alenafilimonova98@gmail.com>
2024-12-01 23:30:53 +0500

    Разработка страницы Confirmation. Филимонова

---
+++ Pages/Confirmation.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegIN_Filimonova.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Confirmation.xaml
+    /// </summary>
+    public partial class Confirmation : Page
+    {
+        public Confirmation()
+        {
+            InitializeComponent();
+        }
+
+        private void SetCode(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void SetCode(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SendMail(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenLogin(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
